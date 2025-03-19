c81c95f3174dabd4fc8ea01c6116163c395bd496
manikntapunnm <111174684+manikntapunnm@users.noreply.github.com>
2025-03-19 16:42:23 +0530

    Update AnagramChecker.java

--- anagram/AnagramChecker.java
+++ anagram/AnagramChecker.java
@@ -1,24 +1,16 @@
 import java.util.Arrays;
 
 public class AnagramChecker {
-    // Method to check if two strings are anagrams
     public static boolean areAnagrams(String str1, String str2) {
-        // Remove spaces and convert to lowercase
         str1 = str1.replaceAll("\\s", "").toLowerCase();
         str2 = str2.replaceAll("\\s", "").toLowerCase();
-
-        // If lengths are different, they can't be anagrams
         if (str1.length() != str2.length()) {
             return false;
         }
-
-        // Convert strings to character arrays and sort
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-
-        // Compare sorted arrays
         return Arrays.equals(arr1, arr2);
     }
 }
