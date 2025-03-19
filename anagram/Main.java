b7c17df339ff63b8fa85cc02e2262ead23a5abe5
manikntapunnm <111174684+manikntapunnm@users.noreply.github.com>
2025-03-19 16:42:55 +0530

    Update Main.java

--- anagram/Main.java
+++ anagram/Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Input two strings
         System.out.print("Enter first string: ");
         String str1 = scanner.nextLine();
 
         System.out.print("Enter second string: ");
         String str2 = scanner.nextLine();
-
-        // Call method from AnagramChecker class
         if (AnagramChecker.areAnagrams(str1, str2)) {
             System.out.println("Output: true (They are anagrams)");
         } else {
